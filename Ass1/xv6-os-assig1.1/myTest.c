078c1f30dca8f46fbca4c766efb51fcf78231faa
Madhur Singhal <msinghal1998@gmail.com>
2018-02-15 00:29:35 +0530

    First Assignment done

---
+++ Ass1/xv6-os-assig1.1/myTest.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  printf(1,"sdfsfds");
+  toggle();
+  printf(1,"%d\n\n",add(44,44));
+  exit();
+}
---
+++ Ass1/xv6-os-assig1.1/user_toggle.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+
+int
+main(int argc, char *argv[])
+{
+  // If you follow the naming convetion, system call name will be sys_toggle and you 
+  // call it by calling 
+  toggle();
+
+  exit();
+}
